0514ce948075e09ae4cb65ec02802b32aeb083cb
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-03-19 23:49:34 +0530

    change made

---
+++ 1/helloworld.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+
+// int main(int argc, const char *argv[])
+// {
+//     std::cout << "Hello World!";
+//     return 0;
+// }
+
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    puts("Click on Button");
+    puts("create a new player");
+    puts("add life to player");
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+    printf("Your ID is %d\n", number + 3);
+    return 0;
+}
